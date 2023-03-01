8f009dde995ce8b39bcbd7adc1ebc077c031e924
Abdulrahman Aminu <aminuabdulrahmandayo@gmail.com>
2023-03-01 18:03:15 +0000

    Segments working in Notifications

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,35 +1,42 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class NotificationsActivities:
+
   def run():
-    now = datetime.now(timezone.utc).astimezone()
-    results = [{
-      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'aaminu',
-      'message': 'I am finding #cloudcamp fun. :)',
-      'created_at': (now - timedelta(days=2)).isoformat(),
-      'expires_at': (now + timedelta(days=5)).isoformat(),
-      'likes_count': 5,
-      'replies_count': 1,
-      'reposts_count': 0,
-      'replies': [{
-        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'Worf',
-        'message': 'This is the post!',
-        'likes_count': 0,
-        'replies_count': 0,
+    with xray_recorder.capture('subsegment_name') as subsegment:
+      now = datetime.now(timezone.utc).astimezone()
+
+      results = [{
+        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'aaminu',
+        'message': 'I am finding #cloudcamp fun. :)',
+        'created_at': (now - timedelta(days=2)).isoformat(),
+        'expires_at': (now + timedelta(days=5)).isoformat(),
+        'likes_count': 5,
+        'replies_count': 1,
         'reposts_count': 0,
-        'created_at': (now - timedelta(days=2)).isoformat()
-      }],
-    },
-    {
-      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'lim<3',
-      'message': 'Hey aaminu, what\'s good?',
-      'created_at': (now - timedelta(days=7)).isoformat(),
-      'expires_at': (now + timedelta(days=9)).isoformat(),
-      'likes': 0,
-      'replies': []
-    }
-    ]
-    return results
+        'replies': [{
+          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+          'handle':  'Worf',
+          'message': 'This is the post!',
+          'likes_count': 0,
+          'replies_count': 0,
+          'reposts_count': 0,
+          'created_at': (now - timedelta(days=2)).isoformat()
+        }],
+      },
+      {
+        'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
+        'handle':  'lim<3',
+        'message': 'Hey aaminu, what\'s good?',
+        'created_at': (now - timedelta(days=7)).isoformat(),
+        'expires_at': (now + timedelta(days=9)).isoformat(),
+        'likes': 0,
+        'replies': []
+      }
+      ]
+      seg_dict = {"now": now.isoformat(), "result_len":len(results)}
+      subsegment.put_annotation('nofitications-annotations', seg_dict)
+      return results
