fe794181ba9fc1af5898e5e05d179b8dc4917d8f
Abdulrahman Aminu <aminuabdulrahmandayo@gmail.com>
2023-03-04 14:53:17 +0000

    Frontend Instrumentation fixed

--- frontend-react-js/src/tracing.js
+++ frontend-react-js/src/tracing.js
@@ -1,17 +1,17 @@
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { WebTracerProvider, BatchSpanProcessor } from '@opentelemetry/sdk-trace-web';
+import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { Resource }  from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 
-var OLTP_URL = `${process.env.OTEL_EXPORT_URL}`
-var llp = `${process.env.REACT_APP_BACKEND_URL}`
-console.log(OLTP_URL);
-console.log(llp);
 const exporter = new OTLPTraceExporter({
-  url: OLTP_URL,
+  url: `${process.env.REACT_APP_OLTP_URL}/v1/traces`,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  
 });
-console.log(exporter);
 const provider = new WebTracerProvider({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'frontend-react-js',
@@ -20,4 +20,10 @@ const provider = new WebTracerProvider({
 provider.addSpanProcessor(new BatchSpanProcessor(exporter));
 provider.register({
   contextManager: new ZoneContextManager()
+});
+
+registerInstrumentations({
+  instrumentations: [
+    new DocumentLoadInstrumentation(),
+  ],
 });
