3b6f00eb1fbf7205a431bea8adbcb8595c25a8e2
Abdulrahman Aminu <aminuabdulrahmandayo@gmail.com>
2023-05-04 13:18:03 -0500

    Build Spec update and health check update

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -124,7 +124,7 @@ def rollbar_test():
 
 @app.route('/api/health-check')
 def health_check():
-    return {'success': True}, 200
+    return {'success': True, 'ver': 1}, 200
 
 
 @app.route("/api/message_groups", methods=['GET'])
